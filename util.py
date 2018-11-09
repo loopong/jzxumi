db557936439f3b3a971e22d6516adf50ebf95271
卢鹏 <lupeng@lupengdeMacBook-Air.local>
2018-11-09 14:19:59 +0800

    update

--- util.py
+++ util.py
@@ -3,6 +3,18 @@ import urllib
 import sys
 
 def real_code(code):
-    if len(code)
     if isinstance(code, int)
+        code = str(code)
+    if len(code) <=0 or code is None:
+        return ""
+    if not code.isdigit():
+        return code
+    code = int(code)
+    if code == 100000:
+        return "sz100000"
+    if code > 400000:
+        code = "sh" + str(code)
+    else:
+        code = "sz" + str(code)
+
 def get_current_price(code)
