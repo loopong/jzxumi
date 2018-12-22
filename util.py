e3e5001a6141bfb530fe7900cf3e70f474149f7b
卢鹏 <lupeng@lupengdeMacBook-Air.local>
2018-12-22 19:13:23 +0800

    update

--- util.py
+++ util.py
@@ -3,18 +3,34 @@ import urllib
 import sys
 
 def real_code(code):
-    if isinstance(code, int)
-        code = str(code)
-    if len(code) <=0 or code is None:
-        return ""
-    if not code.isdigit():
-        return code
-    code = int(code)
-    if code == 100000:
-        return "sz100000"
-    if code > 400000:
-        code = "sh" + str(code)
-    else:
-        code = "sz" + str(code)
+    try:
+        if not code:
+            return code
+        if isinstance(code, str):
+            if len(code) > 2 and code[0:2] == "sz" and code["0:2"] == "sh":
+                return code
+        if isinstance(code, int):
+            code = str(code)
+        code = int(code)
+        if code / 300000 > 0:
+            return "sh" + str(code)
+        else:
+            return "sz" + str(code)
+    except Exception as e:
+        print "recode error : %s" % e
 
-def get_current_price(code)
+def get_current_price(code):
+    def getHtml(url):
+        page = urllib.urlopen(url)
+        html = page.read()
+        return html
+    try:
+        code = real_code(code)
+        url = "http://hq.sinajs.cn/list=" + code
+        return getHtml(url).split(",")[3]
+    except Exception as e:
+        print "get price error : %s" % e
+
+
+if __name__ == '__main__':
+    print get_current_price(601006)
